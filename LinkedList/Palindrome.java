a0a25f2cc02ab2b608b78bfc757dfdf8aa7e4fec
Chanchal2318 <133849770+Chanchal2318@users.noreply.github.com>
2024-11-09 15:32:49 +0000

    Palindrome

---
+++ LinkedList/Palindrome.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Palindrome{
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static boolean checkPalindrome(Node head){
+        if(head == null){
+            return false;
+        }
+        // Get Mid
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // Reverse the 2nd half
+        Node curr = slow;
+        Node prev = null;
+        Node next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        while(left.data != right.data ){
+            return false;
+        }
+        left = left.next;
+        right = right.next;
+        return true;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(1);
+        System.out.println(checkPalindrome(head));
+    }
+}
